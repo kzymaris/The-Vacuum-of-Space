438a5bf8a4067350d8ae565eae404ba9f20421ed
WIT\zymarisk <kzymaris@gmail.com>
2023-02-25 13:51:19 -0500

    cooldowns

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,36 +12,43 @@ public class PlayerMovement : MonoBehaviour {
 	bool jump = false;
 	bool slam = false;
 	bool dash = false;
-	float dashTimer = 0;
-	public float dashCooldown = 1f;
-	
+	float dashTimerR = 0;
+	float dashTimerL = 0;
+	float slamTimer = 0;
+	float jumpTimer = 0;
+	public float dashCooldown = .8f;
+
 	// Update is called once per frame
 	void Update () {
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		horizontalMove = Input.GetAxisRaw ("Horizontal") * runSpeed;
 
-		if (Input.GetAxisRaw("Vertical") > 0)
-		{
+		if (Input.GetAxisRaw ("Vertical") > 0 && Time.time > jumpTimer) {
 			jump = true;
-		}
-		else if (Input.GetAxisRaw("Vertical") < 0)
-		{
+			jumpTimer = Time.time + dashCooldown;
+
+		} else if (Input.GetAxisRaw ("Vertical") < 0 && Time.time > slamTimer) {
 			slam = true;
+			slamTimer = Time.time + dashCooldown;
 		}
 
-        if (Input.GetButtonDown("Dash") && Time.time > dashTimer)
-		{
-			dash = true;
-			dashTimer = Time.time + dashCooldown;
+		if (Input.GetButtonDown ("Dash")) {
+			if (Time.time > dashTimerR && controller.m_FacingRight) {
+				dash = true;
+				dashTimerR = Time.time + dashCooldown;
+			}
+			if (Time.time > dashTimerL && !controller.m_FacingRight) {
+				dash = true;
+				dashTimerL = Time.time + dashCooldown;
+			}
 		}
 	}
 
-	void FixedUpdate ()
-	{
+	void FixedUpdate () {
 		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, dash, jump, slam);
+		controller.Move (horizontalMove * Time.fixedDeltaTime, dash, jump, slam);
 		jump = false;
-        dash = false;
+		dash = false;
 		slam = false;
 	}
 }
