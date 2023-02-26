60e75dd6f49bad9bc81df2570e8ccfda2271654b
LAPTOP-EHPP6DD9\aktie <aktierney@gmail.com>
2023-02-26 17:34:08 -0500

    hoverjump and hoverdash!

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -24,6 +24,13 @@ public class CharacterController2D : MonoBehaviour {
 	private Rigidbody2D m_Rigidbody2D;
 	public bool m_FacingRight = true; // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
+	private bool lDashing = false;
+	private bool rDashing = false;
+	[SerializeField] private float dashTime = 0.1f;
+	private float dashTimer;
+	private bool jumping = false;
+	[SerializeField] private float jumpTime = 0.5f;
+	private float jumpTimer;
 
 	[Header ("Events")]
 	[Space]
@@ -67,83 +74,135 @@ public class CharacterController2D : MonoBehaviour {
 
 	}
 
-	public void Move (float move, float verticalMove, bool dash, bool jump, bool slam, bool magnetOn) {
-		gameObject.GetComponent<Rigidbody2D> ().gravityScale = m_Gravity;
-		if (move == 0f && !dash && m_Grounded) {
-			gameObject.GetComponent<Rigidbody2D> ().sharedMaterial = sticky;
-		} else {
-			gameObject.GetComponent<Rigidbody2D> ().sharedMaterial = slippy;
-		}
-
-		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl || m_OnWall) {
-			if ((m_OnWall && magnetOn) && move == 0) {
-				m_Rigidbody2D.velocity = Vector2.zero;
-				gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0f;
+	public void Move (float move, float verticalMove, bool dash, bool jump, bool unjump, bool slam, bool magnetOn) {
+		if (!rDashing && !lDashing && !jumping)
+		{
+			gameObject.GetComponent<Rigidbody2D>().gravityScale = m_Gravity;
+			if (move == 0f && !dash && m_Grounded)
+			{
+				gameObject.GetComponent<Rigidbody2D>().sharedMaterial = sticky;
 			}
-			if ((m_OnWall && magnetOn) && (move > 0 && m_FacingRight || move < 0 && !m_FacingRight || verticalMove != 0)) {
-				float wallSpeed;
-				if (verticalMove != 0) {
-					wallSpeed = verticalMove;
-				} else {
-					wallSpeed = Math.Abs (move);
+			else
+			{
+				gameObject.GetComponent<Rigidbody2D>().sharedMaterial = slippy;
+			}
+
+			//only control the player if grounded or airControl is turned on
+			if (m_Grounded || m_AirControl || m_OnWall)
+			{
+				if ((m_OnWall && magnetOn) && move == 0)
+				{
+					m_Rigidbody2D.velocity = Vector2.zero;
+					gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
+				}
+				if ((m_OnWall && magnetOn) && (move > 0 && m_FacingRight || move < 0 && !m_FacingRight || verticalMove != 0))
+				{
+					float wallSpeed;
+					if (verticalMove != 0)
+					{
+						wallSpeed = verticalMove;
+					}
+					else
+					{
+						wallSpeed = Math.Abs(move);
+					}
+
+					Debug.Log(wallSpeed);
+
+					Vector3 targetVelocity = new Vector2(0, wallSpeed * 10f);
+					m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+				}
+				else
+				{
+					// Move the character by finding the target velocity
+					Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+					// And then smoothing it out and applying it to the character
+					m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+					// If the input is moving the player right and the player is facing left...
+					if (move > 0 && !m_FacingRight)
+					{
+						// ... flip the player.
+						Flip();
+					}
+					// Otherwise if the input is moving the player left and the player is facing right...
+					else if (move < 0 && m_FacingRight)
+					{
+						// ... flip the player.
+						Flip();
+					}
 				}
 
-				Debug.Log (wallSpeed);
-
-				Vector3 targetVelocity = new Vector2 (0, wallSpeed * 10f);
-				m_Rigidbody2D.velocity = Vector3.SmoothDamp (m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-			} else {
-				// Move the character by finding the target velocity
-				Vector3 targetVelocity = new Vector2 (move * 10f, m_Rigidbody2D.velocity.y);
-				// And then smoothing it out and applying it to the character
-				m_Rigidbody2D.velocity = Vector3.SmoothDamp (m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-
-				// If the input is moving the player right and the player is facing left...
-				if (move > 0 && !m_FacingRight) {
-					// ... flip the player.
-					Flip ();
+			}
+			// If the player should jump...
+			if (jump && !(m_OnWall && magnetOn))
+			{
+				// Cancel some or all movement based on the stopping speed
+				if (m_Rigidbody2D.velocity.magnitude < m_StoppingSpeedJump)
+				{
+					m_Rigidbody2D.velocity = Vector2.zero;
 				}
-				// Otherwise if the input is moving the player left and the player is facing right...
-				else if (move < 0 && m_FacingRight) {
-					// ... flip the player.
-					Flip ();
+				else
+				{
+					m_Rigidbody2D.velocity = m_Rigidbody2D.velocity - m_StoppingSpeedJump * m_Rigidbody2D.velocity.normalized;
+				}
+
+				// Add a vertical force to the player.
+				m_Grounded = false;
+				jumping = true;
+				jumpTimer = Time.time + jumpTime;
+			}
+
+			if (!m_Grounded && slam && !(m_OnWall && magnetOn))
+			{
+				// Cancel some or all movement based on the stopping speed
+				if (m_Rigidbody2D.velocity.magnitude < m_StoppingSpeedDash)
+				{
+					m_Rigidbody2D.velocity = Vector2.zero;
+				}
+				else
+				{
+					m_Rigidbody2D.velocity = m_Rigidbody2D.velocity - m_StoppingSpeedDash * m_Rigidbody2D.velocity.normalized;
+				}
+				m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce * -1));
+			}
+
+			if (dash)
+			{
+				// Cancel some or all movement based on the stopping speed
+				if (m_Rigidbody2D.velocity.magnitude < m_StoppingSpeedDash)
+				{
+					m_Rigidbody2D.velocity = Vector2.zero;
+				}
+				else
+				{
+					m_Rigidbody2D.velocity = m_Rigidbody2D.velocity - m_StoppingSpeedDash * m_Rigidbody2D.velocity.normalized;
+				}
+				rDashing = m_FacingRight;
+				lDashing = !m_FacingRight;
+				dashTimer = Time.time + dashTime;
+			}
+		}
+		else
+		{
+			if (rDashing || lDashing)
+			{
+				m_Rigidbody2D.velocity = new Vector2(rDashing ? m_DashForce : -1 * m_DashForce, m_Rigidbody2D.velocity.y);
+				if (Time.time > dashTimer)
+				{
+					rDashing = false;
+					lDashing = false;
+					m_Rigidbody2D.velocity = Vector2.zero;
 				}
 			}
-
-		}
-		// If the player should jump...
-		if (jump && !(m_OnWall && magnetOn)) {
-			// Cancel some or all movement based on the stopping speed
-			if (m_Rigidbody2D.velocity.magnitude < m_StoppingSpeedJump) {
-				m_Rigidbody2D.velocity = Vector2.zero;
-			} else {
-				m_Rigidbody2D.velocity = m_Rigidbody2D.velocity - m_StoppingSpeedJump * m_Rigidbody2D.velocity.normalized;
-			}
-
-			// Add a vertical force to the player.
-			m_Grounded = false;
-			m_Rigidbody2D.AddForce (new Vector2 (0f, m_JumpForce));
-		}
-
-		if (!m_Grounded && slam && !(m_OnWall && magnetOn)) {
-			// Cancel some or all movement based on the stopping speed
-			if (m_Rigidbody2D.velocity.magnitude < m_StoppingSpeedDash) {
-				m_Rigidbody2D.velocity = Vector2.zero;
-			} else {
-				m_Rigidbody2D.velocity = m_Rigidbody2D.velocity - m_StoppingSpeedDash * m_Rigidbody2D.velocity.normalized;
-			}
-			m_Rigidbody2D.AddForce (new Vector2 (0f, m_JumpForce * -1));
-		}
-
-		if (dash) {
-			// Cancel some or all movement based on the stopping speed
-			if (m_Rigidbody2D.velocity.magnitude < m_StoppingSpeedDash) {
-				m_Rigidbody2D.velocity = Vector2.zero;
-			} else {
-				m_Rigidbody2D.velocity = m_Rigidbody2D.velocity - m_StoppingSpeedDash * m_Rigidbody2D.velocity.normalized;
-			}
-			m_Rigidbody2D.AddForce (new Vector2 (m_FacingRight ? m_DashForce : m_DashForce * -1, 0f));
+			if (jumping)
+			{
+                m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, m_JumpForce);
+				if (unjump || Time.time > jumpTimer)
+				{
+					jumping = false;
+				}
+            }
 		}
 	}
 
